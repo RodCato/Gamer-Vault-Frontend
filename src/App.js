9ba875207908334a64f918674708bc4f2cf8b4eb
jricks86 <jricks86@gmail.com>
2023-06-21 14:38:44 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { Routes, Route } from "react-router-dom"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import GameEdit from "./pages/GameEdit"
+import GameIndex from "./pages/CatIndex"
+import GameNew from "./pages/GameNew"
+import GameShow from "./pages/GameShow"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import "./App.css"
 
-function App() {
+import mockGames from "./mockGames"
+
+const App = () => {
+  const [games, setGames] = useState(mockGames)
+
+  console.log(games)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/catindex" element={<GameIndex />} />
+      <Route path="/catshow" element={<GameShow />} />
+      <Route path="/catnew" element={<GameNew />} />
+      <Route path="/catedit" element={<GameEdit />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </>
+)
 }
 
-export default App;
+export default App
